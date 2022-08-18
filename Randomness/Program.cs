00c40eb3211c8545ea53041d93552e0f031467be
Ramanarajan D <ramanarajan.d@msc.com>
2022-08-18 13:00:30 +0530

    Randomness Added

---
+++ Randomness/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            Console.WriteLine(rand.Next());
+            Console.WriteLine(rand.Next(26)); // Lower than 26
+            Console.WriteLine(rand.Next(47,72)); // Between 47 and 72
+                                                 //Console.WriteLine(rand.NextDouble();
+
+            double val = (rand.NextDouble() * (100));
+
+            Console.WriteLine(val);
+
+            for (int j = 0; j < 6; j++)
+                Console.Write(" {0,7} ", rand.Next());
+
+            Console.ReadKey();
+        }
+    }
+}
